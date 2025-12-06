c20f0c9b4db36d4cab5b7e187c16cc29131cf5a3
AshishRastogi123 <rastogiashish836@gmail.com>
2025-12-06 20:08:04 +0530

    optimal

--- Hashing/hashing_optimal.py
+++ Hashing/hashing_optimal.py
@@ -1,21 +1,20 @@
 """
-
 Count the frequency of elements of list m in list n using a hash map.
 1. 0<n[i]<10
 2. n can have 10^8 elements
 3. m can have 10^5 elements
-solution using brute force approach with O(n*m) time complexity
+4. Time complexity should be O(n+m)
 
 
 """
-
 n=[5,3,2,2,1,5,5,3,4,1,2,5,6,7,8,5,4,3,2,1,10]
 m=[10,111,1,9,5,67,2]
+lg=[0,1,2,3,4,5,6,7,8,9,10] # since 0<n[i]<10
+print(lg)
+fs=[0]*11
+for i in n:
+    fs[i]+=1
 hash_map={}
-for i in m:
-    count=0
-    for j in n:
-        if i==j:
-            count+=1
-    hash_map[i]=count
+for j in m:
+    hash_map[j]=fs[j] if j<=10 else 0
 print(hash_map)
