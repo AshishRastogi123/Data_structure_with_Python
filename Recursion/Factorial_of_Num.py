42d0c59470d8dbd33612b38d89d78ad0a234c92b
AshishRastogi123 <rastogiashish836@gmail.com>
2025-12-08 19:41:53 +0530

    own logic reverse array recursion

--- Recursion/Factorial_of_Num.py
+++ Recursion/Factorial_of_Num.py
@@ -1,5 +1,11 @@
 def fact(n):
-    if n==0:
+    if n==1 or n==0:
         return 1
     return n*fact(n-1)
 print(fact(5))
+
+"""Factorial of Num whre from 1 to n
+time complexity O(n)
+space complexity O(n)
+stack space O(n)
+"""
---
+++ Recursion/Reverse_array.py
@@ -0,0 +1,30 @@
+"""
+Reverse an array using recursion where you have two variable left and right and sort the array between them
+let left =2, right=9 and not interfare another 
+
+num=[5,1,9,12,5,8,4,19,10]
+left =2
+right =5
+then output
+[5,1,4,5,8,9,12,19,10]
+
+
+"""
+
+arr=[5,1,9,12,5,8,4,19,10]
+start=2
+end =7
+
+def reverse_array(arr, start, end):
+    if start==end:
+        print(arr)
+        return 
+    p=start
+    while p<end:
+      if arr[start]>arr[p+1]:
+         arr[start],arr[p+1]=arr[p+1],arr[start]
+      p+=1
+
+    reverse_array(arr,start+1,end)
+reverse_array(arr,start,end)
+    
