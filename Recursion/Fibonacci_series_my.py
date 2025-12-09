1dabef1a5dbdfd720ad4a46687259f264a219c21
AshishRastogi123 <rastogiashish836@gmail.com>
2025-12-09 23:25:33 +0530

    fibonacci optimal

--- Recursion/Fibonacci_series_my.py
+++ Recursion/Fibonacci_series_my.py
@@ -8,17 +8,16 @@ k=0
 def fabinoci(n,k,p):
     if n==-1:
         l=p[k-1]
+        print("The fabinoci series is ",p)
+
         print(l)
         return 1
     if k==0:
         p.append(0)
     elif k==1:
-        p.append(1)
-        
+        p.append(1)     
     else:
         p.append(p[k-1]+p[k-2])
-        print(p)
-        print(f"{k} number")
     
     fabinoci(n-1,k+1,p)
 fabinoci(n,k,p)
