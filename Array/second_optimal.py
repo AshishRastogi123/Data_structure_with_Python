4a62f78638953e129d533905f16277813d0fffba
AshishRastogi123 <rastogiashish836@gmail.com>
2025-12-12 12:27:42 +0530

    second largest

---
+++ Array/second_optimal.py
@@ -0,0 +1,13 @@
+"""
+Find the largest element in the array
+"""
+nums=[55,32,-97,99,3,67]
+largest=float("-inf")
+s_largest=float("-inf")
+for i in nums:
+    if i>largest:
+        s_largest=largest
+        largest=i
+    elif i>s_largest and i!=largest:
+        s_largest=i
+print(s_largest,largest)
