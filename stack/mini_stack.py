5db3681dbdffb51938465b4894f81b4da67857d7
AshishRastogi123 <rastogiashish836@gmail.com>
2026-02-10 22:45:48 +0530

    implement mini stack

--- stack/mini_stack.py
+++ stack/mini_stack.py
@@ -6,6 +6,8 @@ void pop() removes the element on the top of the stack.
 int top() gets the top element of the stack.
 int getMin() retrieves the minimum element in the stack.
 You must implement a solution with O(1) time complexity for each function.
+
+
 """
 class MiniStack:
     def __init__(self):
