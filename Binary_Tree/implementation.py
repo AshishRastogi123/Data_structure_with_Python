c37f7b6ba7cd6f7a87c8763c08abea2d62e73b9d
AshishRastogi123 <rastogiashish836@gmail.com>
2026-03-22 18:15:53 +0530

    Binary Tree foundation and implementation

---
+++ Binary_Tree/implementation.py
@@ -0,0 +1,39 @@
+""""
+Binary Tree Structure:
+        Drink
+       /     \\
+    Water    Cock
+    /  \     /  \\
+Bislari Simple Fanta Thumpshup
+"""
+
+
+class Binary_Node:
+    def __init__(self,val):
+        self.val=val
+        self.left=None
+        self.right=None
+
+drink=Binary_Node("Drink")
+water=Binary_Node("water")
+cock=Binary_Node("Cock")
+bislari=Binary_Node("Bislary")
+simple=Binary_Node("Simple")
+fanta=Binary_Node("Fanta")
+thumsup=Binary_Node("Thumpshup")
+
+drink.left=water
+drink.right=cock
+
+water.left=bislari
+water.right=simple
+
+cock.left=fanta
+cock.right=thumsup
+
+print(drink.left)
+print(water)
+print(drink.left.left.val)
+print(drink.left.left.left.val)
+#If node does not create
+#AttributeError: 'NoneType' object has no attribute 'val'
