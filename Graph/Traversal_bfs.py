f4d79fdf54821f7320c425d3c00c906e008ff022
AshishRastogi123 <rastogiashish836@gmail.com>
2026-03-31 12:38:18 +0530

    traversal using breath first search

---
+++ Graph/Traversal_bfs.py
@@ -0,0 +1,40 @@
+"""
+Traversal using breath first search
+
+Link : 
+"""
+from collections import deque
+class Solution:
+    def breath_first_search(self,adj):
+        ans=[]
+        queue=deque()
+        n=len(adj)
+        start=1
+        visited=[0]*n
+        queue.append(start)
+        visited[start]=1
+        while len(queue):
+            e=queue.popleft()
+            ans.append(e)
+            for node in adj[e]:
+                if visited[node]==0:
+                    queue.append(node)
+                    visited[node]=1
+        return ans
+s=Solution()
+n = 9
+
+adjacency_list = [
+    [],
+    [2, 8],
+    [1, 3, 4],
+    [2],
+    [2, 5],
+    [4, 6],
+    [5, 7],
+    [6, 8],
+    [1, 7, 9],
+    [8],
+]
+
+print(s.breath_first_search(adjacency_list))
