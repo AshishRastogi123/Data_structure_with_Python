8442d13575f6425db8525e33237d74e510d3247e
AshishRastogi123 <rastogiashish836@gmail.com>
2026-04-04 11:20:00 +0530

    flood fill using bfs

--- Graph/Flood_fill.py
+++ Graph/Flood_fill.py
@@ -21,6 +21,10 @@ Output: [[2,2,2],[2,2,0],[2,0,1]]
 from collections import deque
 class Solution:
     def flood_fill_using_dfs(self,image, sr, sc,color):
+        """
+        Time Complexity: O(n*m*4)
+        Space Complexity: O(n*m) + O(n*m) stack space in case of all need to change the color and a image copy matrix
+        """
         if image[sr][sc]==color:
             return image
         image_copy=image.copy()
@@ -43,10 +47,33 @@ class Solution:
         self.dfs_flood(i-1,j,color,initial_color,image_copy,rows,cols)
         self.dfs_flood(i,j-1,color,initial_color,image_copy,rows,cols)
 
+    def flood_fill_using_bfs(self,image,sr,sc,color):
+        if image[sr][sc]==color:
+            return image
+        image_copy=image.copy()
+        rows=len(image)
+        cols=len(image[0])
+        initial_color=image_copy[sr][sc]
+        queue=deque()
+        queue.append((sr,sc))
+        while queue:
+            i,j = queue.popleft()
+            image_copy[i][j]=color
+            for x,y in [(1,0),(0,1),(-1,0),(0,-1)]:
+                new_i=i+x
+                new_j=j+y
+                if new_i<0 or new_i>=rows or new_j<0 or new_j>=cols:
+                    continue
+                if image_copy[new_i][new_j]!=initial_color:
+                    continue
+                queue.append((new_i,new_j))
+        return image_copy
+
+
 
 s=Solution()
 image = [[1,1,1],[1,1,0],[1,0,1]]
 sr = 1
 sc = 1
 color = 2
-print(s.flood_fill_using_dfs(image,sr,sc,color))
+print(s.flood_fill_using_bfs(image,sr,sc,color))
